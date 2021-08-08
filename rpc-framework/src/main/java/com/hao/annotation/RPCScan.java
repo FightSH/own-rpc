927a6c9d279048b2bf843a0d7d41e378cca1ffbb
hao <shenhao189@foxmail.com>
2021-08-08 18:06:31 +0800

    注解修改，引入spring进行管理

--- rpc-framework/src/main/java/com/hao/annotation/RPCScan.java
+++ rpc-framework/src/main/java/com/hao/annotation/RPCScan.java
@@ -1,11 +1,15 @@
 package com.hao.annotation;
 
+import com.hao.spring.CustomScannerRegistrar;
+import org.springframework.context.annotation.Import;
+
 import java.lang.annotation.*;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE})
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Inherited
+@Import(CustomScannerRegistrar.class)
 public @interface RPCScan {
 
     String[] basePackage();
